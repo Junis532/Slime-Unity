8f98a1ee82406eaea103d4579786bcfbb4d51804
WalFoo <75172436+Junis532@users.noreply.github.com>
2025-09-01 14:36:18 +0900

    Slime v0.7.9

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public Button pauseButton;
+    public Button resumeButton;
+    public Button leaveButton;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (pauseButton != null)
+        {
+            pauseButton.onClick.AddListener(ToggleUI);
+        }
+        if (resumeButton != null)
+        {
+            resumeButton.onClick.AddListener(ToggleUI);
+        }
+        if (leaveButton != null)
+        {
+            leaveButton.onClick.AddListener(OnLeaveClicked);
+        }
+    }
+
+    void ToggleUI()
+    {
+        if (pauseMenu != null)
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+
+        }
+        if (pauseMenu.activeSelf)
+        {
+            Time.timeScale = 0f; // Pause the game
+            GameManager.Instance.playerController.canMove = false;
+        }
+        else
+        {
+            Time.timeScale = 1f; // Resume the game
+            GameManager.Instance.playerController.canMove = true;
+        }
+    }
+
+    void OnLeaveClicked()
+    {
+        LoadingManager.LoadScene("MainMenu");
+    }
+}
