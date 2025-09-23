728713b5493495a970e1b8fbef7cf42b789a4cd8
WalFoo <75172436+Junis532@users.noreply.github.com>
2025-09-24 02:32:40 +0900

    Slime v0.9.9.39

--- Assets/Code/Scripts/ScriptableObject/MiddleBoss/MiddleBoss1Statss.cs
+++ Assets/Code/Scripts/ScriptableObject/MiddleBoss/MiddleBoss1Statss.cs
@@ -18,7 +18,7 @@ public class MiddleBoss1Stats : ScriptableObject
     public void ResetStats()
     {
         speed = 0f;
-        maxHP = 30000;
+        maxHP = 25000;
         currentHP = maxHP;
         attack = 100;
     }
